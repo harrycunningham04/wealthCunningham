28a1282e6f4fce080a2e8ed066f5b45a323fd6a3
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-03 13:12:17 +0000

    hero banner section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,68 @@
 "use client";
 
-import React from "react";
+import Link from "next/link";
+import React, { useRef, useEffect } from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
 
 const Hero = () => {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="pb-20 px-4">
-      <div>
-        <h1>
+      <div className="container mx-auto text-center">
+        <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
           Manage your Finances <br /> with Intelligence
         </h1>
-        <p>
-            An AI-powered financial management platofmr that helps you track, analyse and optimise your spending with real time insights.
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+          An AI-powered financial management platofmr that helps you track,
+          analyse and optimise your spending with real time insights.
         </p>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button className="px-8" size="lg">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button variant="outline" className="px-8" size="lg">
+              Demo
+            </Button>
+          </Link>
+        </div>
+
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner.png"
+              alt="Dashboard"
+              width={1280}
+              height={720}
+              className="rounded-lg shadow-2xl border mx-auto"
+            />
+          </div>
+        </div>
+
+        
       </div>
     </div>
   );
