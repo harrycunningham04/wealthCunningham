d25a7cc382c135a694ffbc0499e1cafef36cdee3
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-04 14:25:43 +0000

    seed is practice data so that i can create my graphs with

--- actions/accounts.js
+++ actions/accounts.js
@@ -61,3 +61,49 @@ export async function updateDefaultAccount(accountId) {
     return { success: false, error: error.message };
   }
 }
+
+export async function getAccountWithTransactions(accountId) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Not Authenticated");
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      clerkUserId: userId,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const account = await db.account.findUnique({
+    where: {
+      id: accountId,
+      userId: user.id,
+    },
+    include: {
+      transactions: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+      _count: {
+        select: {
+          transactions: true,
+        },
+      },
+    },
+  });
+
+  if (!account) {
+    return null;
+  }
+
+  return {
+    ...serialiseTransaction(account),
+    transactions: account.transactions.map(serialiseTransaction),
+  };
+}
