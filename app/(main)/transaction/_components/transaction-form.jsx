c660a6cb0bd9ad7e370820a0b1c3e0567321203c
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-06 18:43:18 +0000

    transaction create action

---
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const AddTransactionForm = () => {
+  return <div> AddTransactionForm</div>;
+};
+
+export default AddTransactionForm;
--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { getUserAccounts } from "@/actions/dashboard";
+import { defaultCategories } from "@/data/categories";
+import AddTransactionForm from "../_components/transaction-form";
+import React from "react"; 
 
-const page = () => {
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
 
-export default page
+      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+    </div>
+  );
+};
+
+export default AddTransactionPage;
