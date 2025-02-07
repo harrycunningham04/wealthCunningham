7634b743d082fa56bb035528eef1ee62c618ba55
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-07 13:05:58 +0000

    type, account, amount, category, start of date done

--- app/(main)/transaction/_components/transaction-form.jsx
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -13,9 +13,27 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import CreateAccountDrawer from "@/components/CreateAccountDrawer";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 
 const AddTransactionForm = ({ accounts, categories }) => {
-  useForm({
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    getValues,
+    reset,
+  } = useForm({
     resolver: zodResolver(transactionSchema),
     defaultValues: {
       type: "EXPENSE",
@@ -33,22 +51,127 @@ const AddTransactionForm = ({ accounts, categories }) => {
     data: transactionResult,
   } = useFetch(createTransaction);
 
+  const type = watch("type");
+  const isRecurring = watch("isRecurring");
+  const date = watch("date");
+
+  const filteredCategories = categories.filter(
+    (category) => category.type === type
+  );
+
   return (
-    <form>
+    <form className="space-y-6">
       {/*AI Recipt Scanner*/}
 
-      <div>
-        <label>Type:</label>
-        <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Theme" />
+      {/*Form to enter details*/}
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Type:</label>
+        <Select
+          onValueChange={(value) => setValue("type", value)}
+          defaultValue={type}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select type" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
+            <SelectItem value="EXPENSE">Expense</SelectItem>
+            <SelectItem value="INCOME">Income</SelectItem>
           </SelectContent>
         </Select>
+
+        {errors.type && (
+          <p className="text-sm text-red-500">{errors.type.message}</p>
+        )}
+      </div>
+
+      <div className="grid gap-6 md:grid-cols-2">
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Amount:</label>
+          <Input
+            type="Number"
+            step="0.01"
+            placeholder="£0.00"
+            {...register("amount")}
+          />
+
+          {errors.amount && (
+            <p className="text-sm text-red-500">{errors.amount.message}</p>
+          )}
+        </div>
+
+        <div className="space-y-2">
+          <label className="text-sm font-medium">Account:</label>
+          <Select
+            onValueChange={(value) => setValue("type", value)}
+            defaultValue={getValues("accountId")}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="Select account" />
+            </SelectTrigger>
+            <SelectContent>
+              {accounts.map((account) => (
+                <SelectItem key={account.id} value={account.id}>
+                  {account.name} (${parseFloat(account.balance).toFixed(2)})
+                </SelectItem>
+              ))}
+              <CreateAccountDrawer>
+                <Button
+                  variant="ghost"
+                  className="w-full select-none items-center text-sm outline-none"
+                >
+                  Create Account
+                </Button>
+              </CreateAccountDrawer>
+            </SelectContent>
+          </Select>
+
+          {errors.accountId && (
+            <p className="text-sm text-red-500">{errors.accountId.message}</p>
+          )}
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Category:</label>
+        <Select
+          onValueChange={(value) => setValue("category", value)}
+          defaultValue={getValues("category")}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Select cateogry" />
+          </SelectTrigger>
+          <SelectContent>
+            {filteredCategories.map((category) => (
+              <SelectItem key={category.id} value={category.id}>
+                {category.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        {errors.category && (
+          <p className="text-sm text-red-500">{errors.category.message}</p>
+        )}
+      </div>
+
+      <div className="space-y-2">
+        <label className="text-sm font-medium">Date:</label>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline"
+            className="w-full pl-3 text-left font-normal">
+                {" "}
+                {date ? format(date, "PPP") : <span>Select a date</span>}
+                <CalendarIcon className="ml-auto size-4 opacity-50 " />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>Place content for the popover here.</PopoverContent>
+        </Popover>
+
+        {errors.date && (
+          <p className="text-sm text-red-500">{errors.date.message}</p>
+        )}
       </div>
     </form>
   );
