5c1149586ea4c01784eb15a76b19de0f1de87596
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-12 10:34:58 +0000

    middleware spelling

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 const aj = arcjet({
-  key: process.env.ARCJECT_KEY,
+  key: process.env.ARCJET_KEY,
   rules: [
     shield({
       mode: "LIVE",
