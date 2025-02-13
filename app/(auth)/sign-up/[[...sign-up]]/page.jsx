6ac6e4b63e3756b418f3338168f56ca71d193cc2
Harry Cunnigham <cunninghamharry09@gmail.com>
2025-02-13 18:27:05 +0000

    fixed

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,16 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 import React from "react";
-import { useRouter } from "next/router";
 
 const page = () => {
-  const router = useRouter();
 
-  const handleSignUpSuccess = () => {
-    // Redirect to dashboard
-    router.push("/dashboard");
-  };
 
-  return <SignUp onSuccess={handleSignUpSuccess} />;
+  return <SignUp/>;
 };
 
 export default page;
